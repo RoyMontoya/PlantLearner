9fe419e16fd76dcda3fb5f5e6c5f67a5f4e44c81
Roy Montoya <roymontoya89@gmail.com>
2018-03-14 22:05:51 -0600

    initial commit

---
+++ app/src/main/java/com/example/roy/plantlearner/FlashCardActivity.kt
@@ -0,0 +1,18 @@
+package com.example.roy.plantlearner
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class FlashCardActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_flash_card)
+    }
+
+
+    fun onClickButton3(v: View){
+
+    }
+}
---
+++ app/src/main/java/com/example/roy/plantlearner/dto/Plant.kt
@@ -0,0 +1,8 @@
+package com.example.roy.plantlearner.dto
+
+/**
+ * Created by Roy on 3/14/18.
+ */
+
+class Plant(var guid: Int, var genus: String, var species: String,
+            var cultivar: String, var common: String, var height: Int = 0)
